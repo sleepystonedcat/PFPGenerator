f62311144b46e9cb6b4aefefb6247b7c2d9e0328
sleepystonedcat <sleepystonedcat@da.box>
2024-06-20 03:17:47 -0700

    Create webamp.js

---
+++ webamp.js
@@ -0,0 +1,232 @@
+      const Webamp = window.Webamp;
+      new Webamp({
+      
+          initialSkin: {
+        url: 
+      // Find skins https://skins.webamp.org - download them and          host them on your website!
+     "https://dungeon.nekoweb.org/MountainDew.wsz"
+    },
+              availableSkins: [
+          {
+            url: "https://dungeon.nekoweb.org/Microchip_2.wsz",
+            name: "Microchip_2.wsz",
+          },
+          {
+            url: "https://dungeon.nekoweb.org/MountainDew.wsz",
+            name: "MountainDew.wsz",
+          },
+                    {
+            url: "https://dungeon.nekoweb.org/John_Wilcox.wsz",
+            name: "John_Wilcox.wsz",
+          },
+                              {
+            url: "https://dungeon.nekoweb.org/Cryostasis.wsz",
+            name: "Cryostasis.wsz",
+          },
+                              {
+            url: "https://dungeon.nekoweb.org/amstrad_skullz.wsz",
+            name: "amstrad_skullz.wsz",
+          },
+                              {
+            url: "https://dungeon.nekoweb.org/Matrix_Code_K.wsz",
+            name: "Matrix_Code_K.wsz",
+          },
+                                        {
+            url: "https://dungeon.nekoweb.org/XBOX_MasterAmp.wsz",
+            name: "XBOX_MasterAmp.wsz",
+          },
+                              {
+            url: "https://dungeon.nekoweb.org/LED.wsz",
+            name: "LED.wsz",
+          },
+          
+         ],
+      
+        zIndex: 99999,
+        initialTracks: [{
+            metaData: {
+              title: "Agoraphobia",
+              artist: "strxwberrymilk",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/9yb226.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "My Head Hurts",
+              artist: "strxwberrymilk",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/tfamxo.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "I'll Go Home Then",
+              artist: "strxwberrymilk",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/0x5x0g.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "Under Your Spell",
+              artist: "Snow Strippers",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/lxgm7o.mp3",
+            duration: 322.612245,
+          },
+          {
+            metaData: {
+              title: "Just Your Doll",
+              artist: "Snow Strippers",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/ht3b65.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "lain",
+              artist: "C4FF31N3",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/32ruv3.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "i kin daffodil fettuccine from psycholonials so fucking hard",
+              artist: "C4FF31N3",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/rp4c0v.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "EARTHBOUND",
+              artist: "black balloons",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/bh1cnj.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "EMERALD HANDS",
+              artist: "black balloons",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/7l568f.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "session 0777777777777r21qffffffff jkopüswedxywy w333333r",
+              artist: "expiry date",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/bjx0it.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "CHALICE OF MIND",
+              artist: "Yabujin",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/xu4198.mp3",
+            duration: 190.093061,
+          },
+          {
+            metaData: {
+              title: "Let's All Love Lain",
+              artist: "TOKYOPILL",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:b
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/uthupf.mp3",
+            duration: 190.093061,
+          },
+           {
+            metaData: {
+              title: "moment (feat. imogen heap)",
+              artist: "vierre cloud",
+            },
+            // NOTE: Your audio file must be served from the same domain as your HTML
+            // file, or served with permissive CORS HTTP headers:b
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+            url: "https://files.catbox.moe/1k7eim.mp3",
+            duration: 190.093061,
+          },
+
+        ],
+        __butterchurnOptions: {
+          importButterchurn: () => Promise.resolve(window.butterchurn),
+          getPresets: () => {
+            const presets = window.butterchurnPresets.getPresets();
+            return Object.keys(presets).map((name) => {
+              return {
+                name,
+                butterchurnPresetObject: presets[name]
+              };
+            });
+          },
+          butterchurnOpen: true
+        },
+        __initialWindowLayout: {
+          main: {
+            position: {
+              x: 0,
+              y: 0
+            }
+          },
+          equalizer: {
+            position: {
+              x: 0,
+              y: 116
+            }
+          },
+          playlist: {
+            position: {
+              x: 0,
+              y: 232
+            },
+            size: [0, 4]
+          },
+          milkdrop: {
+            position: {
+              x: 275,
+              y: 0
+            },
+            size: [7, 12]
+          }
+        }
+      }).renderWhenReady(document.getElementById('app'));
